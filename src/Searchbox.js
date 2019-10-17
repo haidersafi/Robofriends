2201c9d6266489114fedcc00ee653b0abff095a6
Safi Saqlain <safisaqlain@gmail.com>
2019-10-17 14:58:04 +0500

    adding new files

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 const Searchbox=({searchChange})=>{
 	return(
-		<div className='pa2 '><input onChange={searchChange} className=' tc f3 pa3 ba b--green bg-lightest-green' type='search' placeholder='Search Robots'/></div>)
+		<div><input onChange={searchChange} className=' tc f3 pa2 ba b--green bg-lightest-green' type='search' placeholder='Search Robots'/></div>)
 }
 export default Searchbox;
