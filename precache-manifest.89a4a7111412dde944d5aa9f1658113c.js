dccf0997d0c98ca0e8c6fb7fdafe1ed5c1b4a759
Safi Saqlain <safisaqlain@gmail.com>
2019-10-17 13:18:24 +0500

    Updates

---
+++ precache-manifest.89a4a7111412dde944d5aa9f1658113c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533089a38979154fb999a18bae1940c1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c23c2e333c61e8502146",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "595d94e014c43879678d",
+    "url": "/robofriends/static/css/main.005e932d.chunk.css"
+  },
+  {
+    "revision": "c23c2e333c61e8502146",
+    "url": "/robofriends/static/js/2.8c5dd8f7.chunk.js"
+  },
+  {
+    "revision": "595d94e014c43879678d",
+    "url": "/robofriends/static/js/main.bedb49da.chunk.js"
+  },
+  {
+    "revision": "fb40df7bc50ed9a881ea",
+    "url": "/robofriends/static/js/runtime-main.274c3418.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
